9e51bdea32e06a912bb942b69f9011987f09ad12
RainerEnjoyer <justus.menn@web.de>
2025-12-30 18:41:50 +0100

    Create alternative distributions.R

---
+++ alternative distributions.R
@@ -0,0 +1,40 @@
+#script to generate alternative distributions
+
+# global parameters
+
+library(tidyverse)
+set.seed(123)
+n <- 10000 # 10000 observations in the population dataframe
+
+# version 5/3: Lognormal (baseline)
+m <- 2500
+v <- 800^2
+
+sdlog  <- sqrt(log(1 + v / m^2))
+meanlog <- log(m) - 0.5 * sdlog^2
+
+dat_inc <- rlnorm(n, meanlog, sdlog)|> 
+  data.frame()
+
+names(dat_inc)[1] <- "Lognormal"
+
+plot_skewed <- dat_inc |> 
+  ggplot(aes(x = skewed)) +
+  geom_histogram()
+
+# version 6: Gamma (controlled skewness)
+
+
+# version 7: Pareto (tail stress test)
+alphas <- c(1.5, 2.5, 2.0)
+rpareto_mean <- function(n, alpha, mean) {
+  xm <- mean * (alpha - 1) / alpha
+  xm * runif(n)^(-1 / alpha)
+}
+dat_inc$pareto1 <- rpareto_mean(n, alpha = 2.2, mean = 2500)
+
+for (i in seq_along(alphas)){
+  dat_inc[[paste0("pareto_a", alphas[i])]] <-  rpareto_mean(n, alpha = alphas[i], mean = 2500)
+} 
+
+# version 8: Mixtures (body + heavy tail)
