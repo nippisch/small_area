5ab39ed286dd8d86b1540a62cd13b4186d49c310
RainerEnjoyer <justus.menn@web.de>
2026-01-19 20:20:19 +0100

    Update evaluation.R

--- evaluation.R
+++ evaluation.R
@@ -3,67 +3,239 @@ library(tidyverse)
 
 
 plot_data <- bind_rows(
-  dis1 = dat_var_1,
-  dis2 = dat_var_2,
-  dis3 = dat_var_3,
-  dis4 = dat_var_4,
+  df1 = dat_var_1,
+  df2 = dat_var_2,
+  df3 = dat_var_3,
+  df4 = dat_var_4,
   .id = "distribution"
 )
 
-#plotting variances per iteration
 
-boot_per_iteration <-
-plot_data |>
-  filter(domain == "d1") |>
-  ggplot(aes(x = R, y = boot, colour = distribution)) +
-  geom_line() +
-  theme_minimal() +
-  ylab("bootstrap variance") +
-  xlab("iterations")
+#Calculations
 
-jack_per_iteration <-
-  plot_data |>
-  filter(domain == "d1") |>
-  ggplot(aes(x = R, y = jack, colour = distribution)) +
-  geom_line() +
-  theme_minimal() +
-  ylab("jackknife variance") +
-  xlab("iterations")
+# calculating relative biases
+dat_fin$rel_bias_boot <- dat_fin$bias_boot / dat_fin$var_mc
+dat_fin$rel_bias_jack <- dat_fin$bias_jack / dat_fin$var_mc
 
-#plotting bias for one chosen distribution and all four domain sizes
+# calculating relative RSMEs
+dat_fin$rel_RMSE_boot <- dat_fin$RSME_boot / dat_fin$var_mc
+dat_fin$rel_RMSE_jack <- dat_fin$RSME_jack / dat_fin$var_mc
+
+
+#Plots
+
+#Plotting
+
+
+#plotting bias for chosen distributions and all four domain sizes
 
 pivoted_data <- dat_fin |>
-  pivot_longer(cols = c("bias_boot", "bias_jack"),
-               names_to = "method",
-               values_to = "bias")
+  pivot_longer(
+    cols = c("bias_boot", "bias_jack"),
+    names_to = "method",
+    values_to = "bias"
+  )
 
-plot_bias <- pivoted_data |> filter(df == 3) |> #choose distribution
-  ggplot(aes(x=domain, y=bias, colour = method))+
+plot_bias_df1 <- pivoted_data |> filter(df == 1) |>
+  ggplot(aes(x = domain, y = bias, colour = method)) +
+  geom_line() +
+  geom_point() +
+  theme_minimal() +
+  ggtitle(label = "Uniform") +
+  ylab("bias compared to MC-benchmark")
+
+plot_bias_df2 <- pivoted_data |> filter(df == 2) |>
+  ggplot(aes(x = domain, y = bias, colour = method)) +
+  geom_line() +
+  geom_point() +
+  theme_minimal() +
+  ggtitle(label = "Gamma") +
+  ylab("bias compared to MC-benchmark")
+
+plot_bias_df3 <- pivoted_data |> filter(df == 3) |>
+  ggplot(aes(x = domain, y = bias, colour = method)) +
+  geom_line() +
+  geom_point() +
+  theme_minimal() +
+  ggtitle(label = "Lognormal") +
+  ylab("bias compared to MC-benchmark")
+
+plot_bias_df4 <- pivoted_data |> filter(df == 4) |>
+  ggplot(aes(x = domain, y = bias, colour = method)) +
+  geom_line() +
+  geom_point() +
+  theme_minimal() +
+  ggtitle(label = "Dagum") +
+  ylab("bias compared to MC-benchmark")
+
+ggpubr::ggarrange(
+  plot_bias_df1,
+  plot_bias_df2,
+  plot_bias_df3,
+  plot_bias_df4,
+  nrow = 2,
+  ncol = 2
+)
+
+#plotting relative bias for all four domain sizes
+
+pivoted_data <- dat_fin |>
+  pivot_longer(cols = c("rel_bias_boot", "rel_bias_jack"),
+               names_to = "method",
+               values_to = "rel_bias")
+
+plot_rel_bias_df1 <- pivoted_data |> filter(df == 1) |> 
+  ggplot(aes(x=domain, y=rel_bias, colour = method))+
   geom_line()+
   geom_point()+
   theme_minimal()+
-  ylab("bias compared to MC-benchmark")
-plot_bias
+  ggtitle(label = "Uniform") +
+  ylab("relative bias compared to MC-benchmark")
 
-#plotting bias for all distributions for one chosen domain
+plot_rel_bias_df2 <- pivoted_data |> filter(df == 2) |> 
+  ggplot(aes(x=domain, y=rel_bias, colour = method))+
+  geom_line()+
+  geom_point()+
+  theme_minimal()+
+  ggtitle(label = "Gamma") +
+  ylab("relative bias compared to MC-benchmark")
 
-pivoted_data <- dat_fin |>
-  pivot_longer(cols = c("bias_boot", "bias_jack"),
-               names_to = "method",
-               values_to = "bias")
+plot_rel_bias_df3 <- pivoted_data |> filter(df == 3) |> 
+  ggplot(aes(x=domain, y=rel_bias, colour = method))+
+  geom_line()+
+  geom_point()+
+  theme_minimal()+
+  ggtitle(label = "Lognormal") +
+  ylab("relative bias compared to MC-benchmark")
 
-#assign distribution names (adjust if other distributions are chosen)
-pivoted_data$df <- recode(
-  pivoted_data$df,
-  `1` = "uniform",
-  `2` = "gamma",
-  `3` = "lognormal",
-  `4` = "jenkins"
+plot_rel_bias_df4 <- pivoted_data |> filter(df == 4) |> 
+  ggplot(aes(x=domain, y=rel_bias, colour = method))+
+  geom_line()+
+  geom_point()+
+  theme_minimal()+
+  ggtitle(label = "Dagum") +
+  ylab("relative bias compared to MC-benchmark")
+
+ggpubr::ggarrange(
+  plot_rel_bias_df1,
+  plot_rel_bias_df2,
+  plot_rel_bias_df3,
+  plot_rel_bias_df4,
+  nrow = 2,
+  ncol = 2
 )
 
-plot_bias <- pivoted_data |> filter(domain == 4) |> #choose domain
-  ggplot(aes(x = df, y = bias, colour = method)) +
-  geom_point(size = 4) +
-  theme_minimal() +
-  ylab("bias compared to MC-benchmark")
-plot_bias
+#plotting relative RMSE for all four domain sizes
+
+pivoted_data <- dat_fin |>
+  pivot_longer(cols = c("rel_RMSE_boot", "rel_RMSE_jack"),
+               names_to = "method",
+               values_to = "rel_RMSE")
+
+plot_rel_rmse_df1 <- pivoted_data |> filter(df == 1) |> 
+  ggplot(aes(x=domain, y=rel_RMSE, colour = method))+
+  geom_line()+
+  geom_point()+
+  theme_minimal()+
+  ggtitle(label = "Uniform") +
+  ylab("relative RMSE compared to MC-benchmark")
+
+plot_rel_rmse_df2 <- pivoted_data |> filter(df == 2) |> 
+  ggplot(aes(x=domain, y=rel_RMSE, colour = method))+
+  geom_line()+
+  geom_point()+
+  theme_minimal()+
+  ggtitle(label = "Gamma") +
+  ylab("relative RMSE compared to MC-benchmark")
+
+plot_rel_rmse_df3 <- pivoted_data |> filter(df == 3) |> 
+  ggplot(aes(x=domain, y=rel_RMSE, colour = method))+
+  geom_line()+
+  geom_point()+
+  theme_minimal()+
+  ggtitle(label = "Lognormal") +
+  ylab("relative RMSE compared to MC-benchmark")
+
+plot_rel_rmse_df4 <- pivoted_data |> filter(df == 4) |> 
+  ggplot(aes(x=domain, y=rel_RMSE, colour = method))+
+  geom_line()+
+  geom_point()+
+  theme_minimal()+
+  ggtitle(label = "Dagum") +
+  ylab("relative RMSE compared to MC-benchmark")
+
+ggpubr::ggarrange(
+  plot_rel_rmse_df1,
+  plot_rel_rmse_df2,
+  plot_rel_rmse_df3,
+  plot_rel_rmse_df4,
+  nrow = 2,
+  ncol = 2
+)
+
+#Distribution of Variance estimates: Bootstrap
+dis_var_boot_df4_d1 <- plot_data |> filter(domain == "d1") |> filter(distribution == "df4")|>
+  ggplot(aes(boot))+
+  geom_histogram()+
+  ggtitle(label = "Uniform") +
+  theme_minimal()
+
+dis_var_boot_df4_d2 <- plot_data |> filter(domain == "d2") |> filter(distribution == "df4")|>
+  ggplot(aes(boot))+
+  geom_histogram()+
+  ggtitle(label = "Gamma") +
+  theme_minimal()
+
+dis_var_boot_df4_d3 <- plot_data |> filter(domain == "d3") |> filter(distribution == "df4")|>
+  ggplot(aes(boot))+
+  geom_histogram()+
+  ggtitle(label = "Lognormal") +
+  theme_minimal()
+
+dis_var_boot_df4_d4 <- plot_data |> filter(domain == "d4") |> filter(distribution == "df4")|>
+ggplot(aes(boot))+
+  geom_histogram()+
+  ggtitle(label = "Dagum") +
+  theme_minimal()
+
+ggpubr::ggarrange(
+  dis_var_boot_df4_d1,
+  dis_var_boot_df4_d2,
+  dis_var_boot_df4_d3,
+  dis_var_boot_df4_d4,
+  nrow = 2,
+  ncol = 2
+)
+#Distribution of Variance estimates: Jackknife
+dis_var_jack_df4_d1 <- plot_data |> filter(domain == "d1") |> filter(distribution == "df4")|>
+  ggplot(aes(jack))+
+  geom_histogram()+
+  ggtitle(label = "Uniform") +
+  theme_minimal()
+
+dis_var_jack_df4_d2 <- plot_data |> filter(domain == "d2") |> filter(distribution == "df4")|>
+  ggplot(aes(jack))+
+  geom_histogram()+
+  ggtitle(label = "Gamma") +
+  theme_minimal()
+
+dis_var_jack_df4_d3 <- plot_data |> filter(domain == "d3") |> filter(distribution == "df4")|>
+  ggplot(aes(jack))+
+  geom_histogram()+
+  ggtitle(label = "Lognormal") +
+  theme_minimal()
+
+dis_var_jack_df4_d4 <- plot_data |> filter(domain == "d4") |> filter(distribution == "df4")|>
+  ggplot(aes(jack))+
+  geom_histogram()+
+  ggtitle(label = "Dagum") +
+  theme_minimal()
+
+ggpubr::ggarrange(
+  dis_var_jack_df4_d1,
+  dis_var_jack_df4_d2,
+  dis_var_jack_df4_d3,
+  dis_var_jack_df4_d4,
+  nrow = 2,
+  ncol = 2
+)
