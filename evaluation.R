da6aac03dbb922cc97550a9887178c6212708728
RainerEnjoyer <justus.menn@web.de>
2026-01-24 17:05:43 +0100

    Plots

--- evaluation.R
+++ evaluation.R
@@ -82,13 +82,79 @@ calc_coverage <- function(dat_var, gini_true, alpha = 0.05) {
     )
 }
 #coverages for chosen distributions
-calc_coverage(dat_var_1, gini_true_uniform)
-calc_coverage(dat_var_2, gini_true_gamma)
-calc_coverage(dat_var_3, gini_true_lognormal)
-calc_coverage(dat_var_4, gini_true_dagum)
+cov_uni <- calc_coverage(dat_var_1, gini_true_uniform)
+cov_gamma <- calc_coverage(dat_var_2, gini_true_gamma)
+cov_lognormal <- calc_coverage(dat_var_3, gini_true_lognormal)
+cov_dagum <- calc_coverage(dat_var_4, gini_true_dagum)
 
+
+cov_uni <- cov_uni |> mutate(domain = recode(
+  domain,
+  "d1" = 1,"d2" = 2,"d3" = 3,"d4" = 4
+))
+cov_gamma <- cov_gamma |> mutate(domain = recode(
+  domain,
+  "d1" = 1,"d2" = 2,"d3" = 3,"d4" = 4
+))
+cov_lognormal <- cov_lognormal |> mutate(domain = recode(
+  domain,
+  "d1" = 1,"d2" = 2,"d3" = 3,"d4" = 4
+))
+cov_dagum <- cov_dagum |> mutate(domain = recode(
+  domain,
+  "d1" = 1,"d2" = 2,"d3" = 3,"d4" = 4
+))
+cov_uni$df <- rep(1)
+cov_gamma$df <- rep(2)
+cov_lognormal$df <- rep(3)
+cov_dagum$df <- rep(4)
+
+dat_cov <- rbind(cov_uni, cov_gamma, cov_lognormal, cov_dagum)
+dat_fin <- merge(dat_fin, dat_cov, by=c("df", "domain"))
 #Plots
 
+#Coverage Plot
+pivoted_data <- dat_fin|>pivot_longer(
+  cols = c("coverage_boot", "coverage_jack"),
+  names_to = "method",
+  values_to = "coverage"
+)
+pivoted_data <- pivoted_data |>
+  dplyr::mutate(method = factor(method, levels = c("coverage_boot", "coverage_jack"),
+                                labels = c("Bootstrap", "Jackknife")),
+                df = factor(
+                  df,
+                  levels = c(1, 2, 3, 4),
+                  labels = c("Uniform", "Gamma", "Lognormal", "Dagum")
+                ))
+
+pivoted_data$covdummy <- as.factor(ifelse(pivoted_data$coverage > 0.95, 1, 0))
+
+
+plot_CI <- pivoted_data |>
+  ggplot(aes(
+    x = domain,
+    y = coverage,
+    fill = covdummy
+  ))+ guides(fill = "none")+
+  geom_col(width = 0.7) +
+  geom_hline(
+    yintercept = 0.95,
+    linewidth = 0.4,
+    colour = "#009E73", alpha=0.6
+  ) +
+  facet_grid(rows = vars(method), cols = vars(df)) +
+  scale_fill_manual(values = c("0" = "firebrick", "1" = "#009E73")) +
+  scale_y_continuous(limits = c(0, 1), breaks = seq(0, 1, by = 0.2))+
+  theme(axis.title.y = element_blank())+
+  labs(title  = "CI coverage rates of the true Gini coefficient")
+
+ggsave(
+  "Plot_CI.pdf",
+  plot_CI,
+  width = 7,
+  height = 5
+)
 #plotting bias for chosen distributions and all four domain sizes
 
 pivoted_data <- dat_fin |>
@@ -139,7 +205,7 @@ ggpubr::ggarrange(
   ncol = 2
 )
 
-#plotting relative bias for all four domain sizes
+#plotting relative bias
 
 pivoted_data <- dat_fin |>
   pivot_longer(cols = c("rel_bias_boot", "rel_bias_jack"),
@@ -187,57 +253,52 @@ ggpubr::ggarrange(
   ncol = 2
 )
 
-#plotting relative RMSE for all four domain sizes
-
+#plotting relative RMSE
 pivoted_data <- dat_fin |>
-  pivot_longer(cols = c("rel_RMSE_boot", "rel_RMSE_jack"),
-               names_to = "method",
-               values_to = "rel_RMSE")
+  pivot_longer(
+    cols = c(rel_RMSE_boot, rel_RMSE_jack),
+    names_to = "method",
+    values_to = "rel_RMSE"
+  ) |>
+  mutate(df = factor(
+    df,
+    levels = 1:4,
+    labels = c("Uniform", "Gamma", "Lognormal", "Dagum")
+  ),
+  method = factor(method, labels = c("Bootstrap", "Jackknife")))
 
-plot_rel_rmse_df1 <- pivoted_data |> filter(df == 1) |> 
-  ggplot(aes(x=domain, y=rel_RMSE, colour = method))+
-  geom_line()+
-  geom_point()+
-  theme_minimal() +
-  ggtitle(label = "Uniform") +
-  ylim(0, 0.7) +
-  ylab("relative RMSE compared to MC-benchmark")
+plot_rel_rmse <- ggplot(pivoted_data,
+                        aes(
+                          x = domain,
+                          y = rel_RMSE,
+                          colour = method
+                        )) +
+  geom_line(linewidth = 0.9) +
+  geom_point(size = 2) +
+  facet_wrap( ~ df, nrow = 2) +
+  scale_colour_manual(name = "Method",
+                      values = c(
+                        "Bootstrap"  = "#E69F00",
+                        "Jackknife"  = "#56B4E9"  
+                      )) +
+  scale_y_continuous(limits = c(0, 0.7),
+                     expand = expansion(mult = c(0, 0.05))) +
+  labs(x = "Domain", y = "Relative RMSE") +
+  theme_bw(base_size = 12) +
+  theme(
+    legend.position = "bottom",
+    strip.text   = element_text(size = 11, face = "bold"),
+    panel.grid.minor = element_blank()
+  )
 
-plot_rel_rmse_df2 <- pivoted_data |> filter(df == 2) |> 
-  ggplot(aes(x=domain, y=rel_RMSE, colour = method))+
-  geom_line()+
-  geom_point()+
-  theme_minimal()+
-  ggtitle(label = "Gamma") +
-  ylim(0, 0.7) +
-  ylab("relative RMSE compared to MC-benchmark")
-
-plot_rel_rmse_df3 <- pivoted_data |> filter(df == 3) |> 
-  ggplot(aes(x=domain, y=rel_RMSE, colour = method))+
-  geom_line()+
-  geom_point()+
-  theme_minimal()+
-  ggtitle(label = "Lognormal") +
-  ylim(0, 0.7) +
-  ylab("relative RMSE compared to MC-benchmark")
-
-plot_rel_rmse_df4 <- pivoted_data |> filter(df == 4) |> 
-  ggplot(aes(x=domain, y=rel_RMSE, colour = method))+
-  geom_line()+
-  geom_point()+
-  theme_minimal()+
-  ggtitle(label = "Dagum") +
-  ylim(0, 0.7) +
-  ylab("relative RMSE compared to MC-benchmark")
-
-ggpubr::ggarrange(
-  plot_rel_rmse_df1,
-  plot_rel_rmse_df2,
-  plot_rel_rmse_df3,
-  plot_rel_rmse_df4,
-  nrow = 2,
-  ncol = 2, common.legend = TRUE
+plot_rel_rmse
+ggsave(
+  "relative_RMSE.pdf",
+  plot_rel_rmse,
+  width = 7,
+  height = 5
 )
+
 
 #Distribution of Variance estimates: Bootstrap
 dis_var_boot_df4_d1 <- plot_data |> filter(domain == "d1") |> filter(distribution == "df4")|>
